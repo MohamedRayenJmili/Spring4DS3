844b89b63ca506494dd2d33fb4f921b5318e2d4e
MohamedRayenJmili <mohamedrayen.jmili@esprit.tn>
2024-01-24 18:23:49 +0100

    initialisation de projet spring

---
+++ src/test/java/tn/spring4ds3/springproj/SpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.spring4ds3.springproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
